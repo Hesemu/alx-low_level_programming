eeee8894c88bc847e6a62d36750c682d8acf9bb1
Herbert <esemuherbert4@gmail.com>
2022-10-13 09:48:34 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# !bin /  bash
+
+#include <stdio.h>
+/**
+*description -- the programs prints with printf
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
