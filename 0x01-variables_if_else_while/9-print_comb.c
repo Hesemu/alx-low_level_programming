f6d65deb39e0dfd6227cbc0108902ffece3a2392
Herbert <esemuherbert4@gmail.com>
2022-10-17 11:15:03 -0700

    Patience and persistence

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
 *main - Entry point of the program
-*Description 'program that prints all possible combinations of single digit
-numbers'
+*Description 'program that prints all possible combinations of single digits'
 *Return: Always (0) success
 */
 int main(void)
