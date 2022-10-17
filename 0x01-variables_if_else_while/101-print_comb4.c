451214383e3fb1f98c0d2d87a8d568e7cb77ace5
Herbert <esemuherbert4@gmail.com>
2022-10-17 16:10:04 -0700

    Success combo in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,17 +11,17 @@ int main(void)
 	int y = 0;
 	int z = 0;
 
-	for (x = 0; x < 7; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = x + 1; y < 8; y++)
+		for (y = x + 1; y < 10; y++)
 		{
-			for (z = y + 1; z < 9; z++)
+			for (z = y + 1; z < 10; z++)
 			{
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
 
-				if (x == 0 && y == 1 && z == 2)
+				if (x == 7 && y == 8 && z == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
