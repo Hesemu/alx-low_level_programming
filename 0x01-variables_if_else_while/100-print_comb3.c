ee9824769589cad9b4a96675c45c6884f30a2996
Herbert <esemuherbert4@gmail.com>
2022-10-17 11:45:43 -0700

    Inventing is a combination of brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*Description 'program that prints all possible different combinations of two di
+gits'
+*Return: Always (0) success
+*/
+int main(void)
+{
+	int x = 0;
+	int y = 0;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 01 && x == 10)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+		}
+		putchar('\n');
+
+	}
+	return (0);
+}
