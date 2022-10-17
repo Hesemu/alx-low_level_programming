fa38c47588d3cfb42cfc798772182275b444b9aa
Herbert <esemuherbert4@gmail.com>
2022-10-17 07:39:41 -0700

    alphabet Soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - entry point of the program
+ *Description 'program prints the alphabet in lowercase except q and e'
+ *Return: Always (0) success
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	if (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+		switch (alphabet)
+		{
+case: 'q';
+case: 'e';
+			continue;
+		}
+	putchar('\n');
+	}
+	return (0);
+}
